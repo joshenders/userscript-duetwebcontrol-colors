93c562e7461b87d69012d2e1f8bd95a9b1c62af4
Josh Enders <josh.enders@gmail.com>
2021-02-11 16:33:32 +0000

    wip

--- duetwebcontrol-colors.user.js
+++ duetwebcontrol-colors.user.js
@@ -2,7 +2,7 @@
 // @name         Duet Web Control Colors
 // @description  Modifies the toolbar color on the Duet Web Control interface
 // @namespace    https://joshenders.com
-// @version      v1.0.1
+// @version      v1.1.0
 // @author       Josh Enders <josh.enders@gmail.com>
 // @updateURL    https://raw.githubusercontent.com/joshenders/userscript-duetwebcontrol-colors/master/duetwebcontrol-colors.user.js
 // @downloadURL  https://raw.githubusercontent.com/joshenders/userscript-duetwebcontrol-colors/master/duetwebcontrol-colors.user.js
@@ -15,18 +15,56 @@
 (function() {
     'use strict';
 
+    // Update with the names of your printers and color choices below
     const config = {
-        "redboi.lan": "#FF4500",
-        "greenboi.lan": "#32CD32",
-        "blueboi.lan": "#1E90FF",
+        // "redboi.lan": "#FF4500",
+        // "greenboi.lan": "#32CD32",
+        // "blueboi.lan": "#1E90FF",
     }
 
-    if (window.location.hostname in config) {
-        const toolbar = document.querySelector(".v-toolbar__content");
-        if (toolbar) {
-            toolbar.style.backgroundColor = config[window.location.hostname];
-        } else {
-            console.warn(".v-toolbar__content not found in current document")
-        }
+    if (!(window.location.hostname in config)) { return; }
+    console.log("duetwebcontrol-colors.user.js active");
+    const color = config[window.location.hostname];
+
+    // toolbar
+    const toolbar = document.querySelector(".v-toolbar__content");
+    if (toolbar) {
+        toolbar.style.backgroundColor = color;
+        console.debug(`Updated toolbar background-color to ${color}`);
+    } else {
+        console.debug(".v-toolbar__content not found in current document")
+    }
+
+    // machine movement buttons
+    const sheet = document.getElementById('vuetify-theme-stylesheet').sheet;
+    if (sheet) {
+        sheet.deleteRule(1);
+        sheet.insertRule(`.v-application .primary { background-color: ${color} !important; border-color: ${color} !important; }`, 1);
+        console.debug("Updated inline style for .v-application.primary");
+    } else {
+        console.debug("vuetify-theme-stylesheet not found in current document");
+    }
+
+    // const buttons = [
+    //    ...document.querySelectorAll(".ml-0.primary"),
+    //    ...document.querySelectorAll(".mx-0.primary")
+    // ];
+    // 
+    // buttons.forEach(item => {
+    //     item.style.backgroundColor = color;
+    //     console.debug(`Updated button background-color to ${color}`);
+    // });
+
+    // macros
+    const macros = document.querySelectorAll("i.blue");
+    if (macros) {
+        macros.forEach(item => {
+            item.classList.remove('blue');
+            item.style.backgroundColor = color;
+            item.style.borderColor = color;
+        });
+        console.debug("Updated macros");
+    } else {
+        console.debug("Macros not found in current document")
     }
 })();
