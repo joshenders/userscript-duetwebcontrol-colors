6ec47ec72c80d3ebc35241b4e77b2c195f3c0590
Josh Enders <josh.enders@gmail.com>
2021-02-11 00:22:31 +0000

    updates colors

--- duetwebcontrol-colors.user.js
+++ duetwebcontrol-colors.user.js
@@ -2,7 +2,7 @@
 // @name         Duet Web Control Colors userscript
 // @description  Modifies the toolbar color on the Duet Web Control interface
 // @namespace    https://joshenders.com
-// @version      v1.0.0
+// @version      v1.0.1
 // @author       Josh Enders <josh.enders@gmail.com>
 // @updateURL    https://raw.githubusercontent.com/joshenders/userscript-duetwebcontrol-colors/master/duetwebcontrol-colors.user.js
 // @downloadURL  https://raw.githubusercontent.com/joshenders/userscript-duetwebcontrol-colors/master/duetwebcontrol-colors.user.js
@@ -17,8 +17,8 @@
 
     const config = {
         "redboi.lan": "#FF4500",
-        "greenboi.lan": "#1E90FF",
-        "blueboi.lan": "#32CD32",
+        "greenboi.lan": "#32CD32",
+        "blueboi.lan": "#1E90FF",
     }
 
     if (window.location.hostname in config) {
